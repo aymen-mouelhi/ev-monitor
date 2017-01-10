8ec124199a584409825baba05eef2c306a32dbef
Aymen Mouelhi <aymen.mouelhi@gmail.com>
2017-01-10 16:05:36 +0100

    call api.ai

--- api/api.js
+++ api/api.js
@@ -1,12 +1,24 @@
-var apiai = require('apiai');
-var Promise = require('promise');
+const apiai = require('apiai');
+const Promise = require('promise');
+const app = apiai("YOUR_ACCESS_TOKEN");
 
 
 module.exports = {
-  process: function(input){
-    return new Promise(function(resolve, reject) {
-      // TODO: get answer from API.ai
-      // TODO: depending on the intent, extra actions may be performed
-    });
-  }
+    process: function(input) {
+        return new Promise(function(resolve, reject) {
+            // TODO: get answer from API.ai
+            // TODO: depending on the intent, extra actions may be performed
+            var request = app.textRequest(input);
+
+            request.on('response', function(response) {
+                console.log(response);
+                resolve(response);
+            });
+            request.on('error', function(error) {
+                console.log(error);
+                reject(error);
+            });
+            request.end();
+        });
+    }
 }
