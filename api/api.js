dafea7ae8e7d1323d1a525c1de143c5f7166b481
Aymen Mouelhi <aymen.mouelhi@gmail.com>
2017-01-10 16:03:05 +0100

    Update dependencies

---
+++ api/api.js
@@ -0,0 +1,12 @@
+var apiai = require('apiai');
+var Promise = require('promise');
+
+
+module.exports = {
+  process: function(input){
+    return new Promise(function(resolve, reject) {
+      // TODO: get answer from API.ai
+      // TODO: depending on the intent, extra actions may be performed
+    });
+  }
+}
--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const request = require('request');
 const app = express();
-
+const API = require('./api/api');
 
 app.set('port', (process.env.PORT || 5000));
 
@@ -18,6 +18,15 @@ app.use(bodyParser.json());
 app.get('/', function(req, res) {
     res.send('Hello, I will help you charge your car');
 });
+
+app.post('/messages', function(req, res){
+  var message = req.query.message;
+  API.process(message).then(function(reply){
+    res.send(reply);
+  }).catch(error){
+    res.send(error);
+  }
+})
 
 // Spin up the server
 app.listen(app.get('port'), function() {
