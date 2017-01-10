c00e8128e9be0561e12b8297370361563b4ae466
Aymen Mouelhi <aymen.mouelhi@gmail.com>
2017-01-10 15:50:34 +0100

    Initial Commit

---
+++ app.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const request = require('request');
+const app = express();
+
+
+app.set('port', (process.env.PORT || 5000));
+
+// Process application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({
+    extended: false
+}));
+
+// Process application/json
+app.use(bodyParser.json());
+
+// Index route
+app.get('/', function(req, res) {
+    res.send('Hello, I will help you charge your car');
+});
+
+// Spin up the server
+app.listen(app.get('port'), function() {
+    console.log('running on port', app.get('port'));
+})
